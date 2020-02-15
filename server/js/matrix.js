20f27d7a18c9002e80d98c6907217b19e10d9659
Suraj Shaw <surajshaw1711@gmail.com>
2020-02-15 23:39:41 +0530

    Base Structure

---
+++ server/js/matrix.js
@@ -0,0 +1,51 @@
+// geting canvas by Boujjou Achraf
+var mbg = document.getElementById("matrix-bg");
+var ctx = mbg.getContext("2d");
+
+//making the canvas full screen
+mbg.height = window.innerHeight;
+mbg.width = window.innerWidth;
+
+
+var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+
+matrix = matrix.split("");
+
+var font_size = 10;
+var columns = mbg.width / font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for (var x = 0; x < columns; x++)
+    drops[x] = 1;
+
+//drawing the characters
+function draw() {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, mbg.width, mbg.height);
+
+    ctx.fillStyle = "red"; //green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for (var i = 0; i < drops.length; i++) {
+        //a random chinese character to print
+        var text = matrix[Math.floor(Math.random() * matrix.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if (drops[i] * font_size > mbg.height && Math.random() > 0.975)
+            drops[i] = 0;
+
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}
+
+setInterval(draw, 35);
+
+
