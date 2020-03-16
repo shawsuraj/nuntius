975c4bcf63e92ad139442c3e0263ccafd622ac39
Suraj Shaw <surajshaw1711@gmail.com>
2020-03-16 17:20:52 +0530

    charset fix

--- server/php/mail.php
+++ server/php/mail.php
@@ -34,7 +34,7 @@ function sendMail($from_email, $to_email, $subject, $content) {
     "Reply-To: ".$from_email."\r\n" .
     "X-Mailer: PHP/" . phpversion();
     $headers .= 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
     @mail($to_email, $subject, $content, $headers);
     
     return "Sent Succesfuly";
